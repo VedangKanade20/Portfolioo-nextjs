651c5a3777e01374b8d26a7146ec0e1fb58877bf
vedang Kanade <vedangk20@gmail.com>
2025-06-14 22:25:42 +0530

    Added a bar

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,23 @@ import { Header } from "@/components/Header";
 export default function Home() {
   return (
     <>
-    
     <div className="flex justify-center ">
       <Header />
     </div>
-    <div>
-      Hello this is home page
+    <div className="flex justify-center">{/* for the main section  */}
+    <div className="flex w-[50%]  bg-gray-900 h-[70vh]">
+    <div className="flex flex-row gap-10">
+      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+        Heyy, I am Vedang Kanade
+      </h2>
+      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+        Aaaaaaaaaaaaaaaaaaaaaaa
+      </h2>
+      </div>
+      <div className="flex flex-row" >
+        
+      </div>
+    </div>
     </div>
     </>
   );
