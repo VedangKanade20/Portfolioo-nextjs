5543c8ff3e4a1ca5bd20c0ec47cb7ffd445e1550
vedang Kanade <vedangk20@gmail.com>
2025-06-15 22:56:24 +0530

    Refactored the files

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,9 @@ import { Header } from "@/components/Header";
 export default function Home() {
   return (
     <>
-    <div className="flex justify-center ">
-      <Header />
-    </div>
+    <Header />
     <div className="flex justify-center">{/* for the main section  */}
-    <div className="flex w-[50%]  bg-gray-900 h-[70vh]">
+    <div className="bg-gray-900 h-[70vh]">
     <div className="flex flex-row gap-10">
       <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
         Heyy, I am Vedang Kanade
@@ -17,9 +15,8 @@ export default function Home() {
         Aaaaaaaaaaaaaaaaaaaaaaa
       </h2>
       </div>
-      <div className="flex flex-row" >
-        
-      </div>
+    
+    
     </div>
     </div>
     </>
