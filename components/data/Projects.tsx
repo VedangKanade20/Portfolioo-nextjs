159dee47b258108528261d53c50fc4fa090c7593
Vedang Kanade <vedangk20@gmail.com>
2026-05-15 01:47:18 +0530

    updated projects

--- components/data/Projects.tsx
+++ components/data/Projects.tsx
@@ -1,10 +1,17 @@
 const projects = [
   {
-    title: "RST Store",
-    type: "E-commerce App",
-    video: "/rststorevideo.mp4",
-    url: "https://rststoree.netlify.app/",
-    source: "https://github.com/VedangKanade20/RSt-Store-E-Com", // optional
+    title: "Crew Canvas",
+    type: "Team Collaboration Tool",
+    video: "/crewcanvasvideo.mp4",
+    url: "https://crew-canvas-client.vercel.app/",
+    source: "https://github.com/VedangKanade20/CrewCanvas", // optional
+  },
+  {
+    title: "Care Connect",
+    type: "Ngo-Donor Platform",
+    video: "/careconvideo.mp4",
+    url: "https://care-connect-eight-theta.vercel.app/",
+    source: "https://github.com/VedangKanade20/Care-Connect", // optional
   },
   {
     title: "Blogosphere",
@@ -14,10 +21,11 @@ const projects = [
     source: "https://github.com/VedangKanade20/BlogoSphere-A-Blog-Web-App", // optional
   },
   {
-    title: "Care Connect",
-    type: "Health Platform",
-    video: "/careconvideo.mp4",
-    url: "https://github.com/VedangKanade20/Care-Connect",
+    title: "Chat Buzz",
+    type: "Real-time Chat App",
+    video: "/chatbuzzvideo.mp4",
+    url: "https://chat-buzz-client.vercel.app/",
+    source: "https://github.com/VedangKanade20/Chat-Buzz",
   },
   {
     title: "Social Media App",
@@ -25,18 +33,20 @@ const projects = [
     video: "/socmedvideo.mp4",
     url: "https://github.com/VedangKanade20/Social-Media-Web-Application-",
   },
+
   {
-    title: "Chat Buzz",
-    type: "Real-time Chat App",
-    video: "/chatbuzzvideo.mp4",
-    url: "https://github.com/VedangKanade20/Chat-Buzz",
+    title: "Salon Management",
+    type: "Salon Management App",
+    video: "/salonmanvideo.mp4",
+    url: "https://salon-managment.vercel.app/",
+    source: "https://github.com/VedangKanade20/Salon-managment", // optional
   },
   {
-    title: "Crew Canvas",
-    type: "Team Collaboration Tool",
-    video: "/crewcanvasvideo.mp4",
-    url: "https://crew-canvas-client.vercel.app/",
-    source: "https://github.com/VedangKanade20/CrewCanvas", // optional
+    title: "RST Store",
+    type: "E-commerce App",
+    video: "/rststorevideo.mp4",
+    url: "https://rststoree.netlify.app/",
+    source: "https://github.com/VedangKanade20/RSt-Store-E-Com", // optional
   },
 ];
 
